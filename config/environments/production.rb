3b2506006a514f6fa395627db6d2fd7fc314bfa8
monamohebbi <monamohebbi9211@gmail.com>
2014-08-03 23:33:47 +0000

    added absolute url path for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Crm::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => "monamohebbi.herokuapp.com" }
 end
